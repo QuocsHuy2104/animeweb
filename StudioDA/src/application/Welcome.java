ffa37e0590ccbc55f717ea8cc47198e86f62a3c2
vaiber2k <vankiet19992003@gmail.com>
2023-10-17 18:27:38 +0700

    GiaoDien

---
+++ StudioDA/src/application/Welcome.java
@@ -0,0 +1,28 @@
+package application;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Welcome extends Application {
+
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Welcome.fxml"));
+		Scene scene = new Scene(root);
+		arg0.setScene(scene);
+		arg0.show();
+		arg0.setResizable(false);
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+
+}
